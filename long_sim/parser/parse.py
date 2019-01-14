39a064f0747b6b92183e6053db70e8d897860563
ZenGround0 <ZenGround0@users.noreply.github.com>
2019-01-13 23:07:33 -0500

    stats are up!

--- long_sim/parser/parse.py
+++ long_sim/parser/parse.py
@@ -12,6 +12,7 @@ class Parser:
         self.ForwardEdges = dict()
         self.AllNodes = set()
         self.RankToNode = dict()
+        self.NodeToRank = dict()
         
         with open(filename, 'r') as dotfile:
             lines = dotfile.readlines()
@@ -57,6 +58,7 @@ class Parser:
             if not self.RankToNode.has_key(rank):
                 self.RankToNode[rank] = []
             self.RankToNode[rank].append(nodeStr)
+            self.NodeToRank[nodeStr] = rank
 
     # Parse a line of the dot file specifying edges (i.e. hash links)
     # ex: "b6485 (m0)" -> "b6440 (m4)";
@@ -91,9 +93,91 @@ class Parser:
                 print(node1 + " -> " + node2)
 
             print("\n")                
+
+
+ 
+
+# MainChain returns the genesis block and the set of blocks in the main chain.
+# Its argument is a dictionary mapping block ids to their parents.
+def makeChain(backwardEdges):
+    mainChain = set()
+    assert('\"head\"' in backwardEdges)
+    mainChain.add('\"head\"')    
+    parents = backwardEdges['\"head\"']
+    assert(len(parents) > 0)
+    for block in parents:
+        mainChain.add(block)
+    
+    while parents[0] in backwardEdges:
+        parents = backwardEdges[parents[0]]
+        assert(len(parents) > 0)
+        for block in parents:
+            mainChain.add(block)
+
+    assert(len(parents) == 1)
+    return parents[0], mainChain
+
+# avgHeadsPerRound returns the mean number of possible mining heads per round.
+# mined in that round.
+def avgHeadsPerRound(rankToNode, numTrials):
+    acc = 0.0
+    for rank in range(0, numTrials):
+        if rank in rankToNode:
+            acc += len(rankToNode[rank]) 
+    return acc / float(numTrials)
+
+# ratioUsefulBlocks returns the ratio of blocks that make it into the main chain
+# to total blocks mined
+def ratioUsefulBlocks(backwardEdges, allBlocks):
+    _, mainChain = makeChain(backwardEdges)
+    return float(len(mainChain)) / float(len(allBlocks))
+
+# countRounds calculates the latest round of a block of a fork.
+def countRounds(forkBlock, forwardEdges, nodeToRank):
+    if forkBlock not in forwardEdges:
+        return nodeToRank[forkBlock]        
+    else:
+        children = forwardEdges[forkBlock]
+        assert(len(children) > 0)
+        counts = []
+        for child in children:
+            counts.append(countRounds(child, forwardEdges, nodeToRank))
+        return max(counts)
         
 
+# avgMaxForkLength calculates the longest branch of every fork that shares a
+# base with a main chain block.  It then averages these lengths together.
+def avgMaxForkRound(forwardEdges, backwardEdges, nodeToRank):
+    genesis, mainChain = makeChain(backwardEdges)
+    maxForkLengths = []
+    mainTipset = set()
+    mainTipset.add(genesis)
+    while '\"head\"' not in mainTipset:
+        nextMain = set()
+        nextForks = set()
+
+        # Enumerate children, separating out into mainChain and forks.
+        for b in mainTipset:
+            next = forwardEdges[b]
+            for a in next:
+                if a in mainChain:
+                    nextMain.add(a)
+                else:
+                    nextForks.add(a)
+
+        # Calculate depth of forks coming from this tipset..
+        for forkBlock in nextForks:
+            maxForkLengths.append(countRounds(forkBlock, forwardEdges, nodeToRank) - nodeToRank[forkBlock] + 1)
+
+        # Continue traversal on next tipset
+        mainTipset = nextMain
+        assert(len(mainTipset) > 0)
+
+    return maxForkLengths
+
+
 if __name__ == '__main__':
-    chain = Parser("output-rounds=10-lbp=1-trial=1.dot")
-    chain.PrintNodeRank()
-    chain.PrintEdgesForwardDirection()
+    chain = Parser("output-rounds=100-lbp=40-trial=1.dot")
+    print(avgHeadsPerRound(chain.RankToNode, 10))
+    print(ratioUsefulBlocks(chain.BackwardEdges, chain.AllNodes))
+    print(avgMaxForkRound(chain.ForwardEdges, chain.BackwardEdges, chain.NodeToRank))
