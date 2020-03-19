cb701fd8fa5e867f4494c0d357c5b930cb6d8560
Henri S <sternhenri@users.noreply.github.com>
2020-03-18 23:52:36 -0400

    added no headstart, changed code whereby attack tying with noon attack can still count as success

--- code/other-sims/general_grinding.py
+++ code/other-sims/general_grinding.py
@@ -8,16 +8,14 @@ att=1/3
 h=1-att
 max_block=20
 
-def nico2():
+def grindWithHS():
     numSims = 100000
     blocksNoGrind=[]
     blocksGrind=[]
     biggerThanExp=0
-    blocksLosts=[]
-    blocksWins = []
-    blocksTotalNoGrind=[]
+    HSBlocksLost=[]
     failedAttempts=0
-    honestBlocks=[]
+    noAttackBlocks=[]
     for i in range(numSims):
         # honest do not grind
         ch1, ch2 = poisson(h*e),poisson(h*e)
@@ -29,68 +27,124 @@ def nico2():
         honWins = ch1 + ch2
         tipset = ch1 + ca 
 
-        # round2 : first trial is on all tipset
+        # grindingRound: adversary tries the second round again
+        # first trial is on the full tipset, then removing one block at a time
         maxGrindedBlock = ca2
-        foundBigger=0 
+        attackSuccess=False
         triedAttack=False
-        # range is exclusive - assume attacker keeps one block
-        # from 1 since I already include the trieal
-        for trial in range(1,tipset):
-            round2 = poisson(att*e)
-            advWin = tipset - trial + round2
-            # attacker doesn't believe he can pull off the attack
+        # from 1 since I already include the first trial to reusing none of the tipset
+        for trial in range(1,tipset+1):
+            grindRound = poisson(att*e)
+            advWin = tipset - trial + grindRound
+            
+            # simple heuristic where attacker doesn't attack unless
+            # they can pull off the attack based on the honest chain's expectation
+            # otherwise risk losing their withheld blocks
             if advWin < ch1 + h*e:
                 continue
 
             triedAttack=True
-            # attacker can pull off the attack
+            # attack success -- we assume better network connectivity from attacker
             if advWin >= honWins:
+                attackSuccess = True
                 # keep the best - that's how much you can win more
-                if maxGrindedBlock < round2:
-                    maxGrindedBlock = round2
-                if maxGrindedBlock > ca2:
-                    foundBigger = 1
+                if grindRound > maxGrindedBlock:
+                    maxGrindedBlock = grindRound
             # attacker tried attack but it failed
 
-        # he successfully ran the attack
-        if foundBigger == 1:
-            # how many blocks more he won
-            blocksWins.append(ca + maxGrindedBlock)
-            blocksTotalNoGrind.append(ca+ca2)
+        # adv successfully ran the attack
+        if attackSuccess:
+            # how many blocks adv won
+            blocksGrind.append(ca + maxGrindedBlock)
+            # alternatively would have won ca2
+            blocksNoGrind.append(ca+ca2)
         # he tried but failed the attack
-        elif foundBigger == 0 and triedAttack:
+        elif not attackSuccess and triedAttack:
             # how many blocks he lost = round1 + round2
-            blocksLosts.append(ca + ca2)
+            HSBlocksLost.append(ca + ca2)
             failedAttempts+=1
         # he did not try the attack
         else:
-            honestBlocks.append(ca+ca2) 
-
-        
-        blocksGrind.append(maxGrindedBlock)
-        blocksNoGrind.append(ca2)
-        biggerThanExp += foundBigger
+            noAttackBlocks.append(ca+ca2)
 
     # all blocks that the adversary gets (or could have had) without  grinding
-    honest_behavior=sum(blocksTotalNoGrind)+sum(blocksLosts)+sum(honestBlocks)
+    honest_behavior=sum(blocksNoGrind)+sum(HSBlocksLost)+sum(noAttackBlocks)
     # all blocks that adv. gets by grinding
-    adv_behavior=sum(blocksWins) + sum(honestBlocks) 
+    adv_behavior=sum(blocksGrind) + sum(noAttackBlocks) 
     adv_attacking = adv_behavior / honest_behavior
     avg_grind=np.average(blocksGrind)
     avg_nogrind=np.average(blocksNoGrind)
-    avg_block_losts=np.average(blocksLosts)
-    avg_additional_block_win=np.average(advWin)
-    avg_blocks_total_nogrind=np.average(blocksTotalNoGrind)
+    avg_block_losts=np.average(HSBlocksLost)
     prop_failed_attempts=failedAttempts/numSims
     print("Average # of blocks when not grinding: {:.2f}".format(avg_nogrind))
     print("Average # of blocks when grinding: {:.2f}".format(avg_grind))
-    print("Proportion of grinded blocks > expected block: {:.2f}".format(biggerThanExp/numSims))
-    print("-> # of blocks losts (grinding): {}".format(sum(blocksLosts)))
-    print("-> # of blocks he won (grinding): {}".format(sum(blocksWins)))
-    print("-> # of blocks normally won (no grinding): {}".format(sum(blocksTotalNoGrind)))
+    print("-> # of blocks losts (grinding): {}".format(sum(HSBlocksLost)))
+    print("-> # of blocks he won (grinding): {}".format(sum(blocksGrind)))
+    print("-> # of blocks normally won (no grinding): {}".format(sum(blocksNoGrind)))
     print("-> proportion of failed attemps: {}".format(prop_failed_attempts))
     print("-> adv. attacking:  {:.3f}".format(adv_attacking))
-    print("Advantage when grinding: {:3f}".format(avg_grind/avg_nogrind))
+    # adv can only attack every second epoch: 
+    # epoch 0, honest blocks are put out, adv decides whether to withhold or not
+    # epoch 1, adv attacks (or not): puts out withheld blocks on grinded tipset
+    # repeat
+    # we account for blocks from both epochs here (preparation and attack)
+
+# in the case without HS the adv can no longer grind on their own blocks 
+# lest they get slashed (they've already put them out).
+def grindWithoutHS():
+    numSims = 100000
+    blocksNoGrind=[]
+    blocksGrind=[]
+    biggerThanExp=0
+    HSBlocksLost=[]
+    failedAttempts=0
+    noAttackBlocks=[]
+    for i in range(numSims):
+        # honest do not grind
+        ch1 = poisson(h*e)
+        ca = poisson(att*e)
+
+        tipset = ch1 + ca 
+        
+        # second round on full tipset
+        ca2 = poisson(att*e)
+        honWins = tipset + poisson(h*e)
+
+        # grindingRound: adversary tries the second round on subsets of tipset
+        
+        # first trial is on the full tipset, then removing one block at a time
+        # until only left with the adversary's (can't drop those)
+        maxGrindedBlock = ca2
+        # from 1 since I already include the first trial to reusing none of the tipset
+        for trial in range(1,ch1 + 1):
+            grindRound = poisson(att*e)
+            advWin = tipset - trial + grindRound
+            
+            # attack success -- we assume better network connectivity from attacker
+            if advWin >= honWins:
+                # keep the best - that's how much you can win more
+                if grindRound > maxGrindedBlock:
+                    maxGrindedBlock = grindRound
+            # attacker tried attack but it failed
+
+        # adv ran the attack
+        blocksGrind.append(ca + maxGrindedBlock)
+        # alternatively would have won ca2
+        blocksNoGrind.append(ca+ca2)
+
+    # all blocks that the adversary gets (or could have had) without  grinding
+    honest_behavior=sum(blocksNoGrind)
+    # all blocks that adv. gets by grinding
+    adv_behavior=sum(blocksGrind)
+    adv_attacking = adv_behavior / honest_behavior
+    avg_grind=np.average(blocksGrind)
+    avg_nogrind=np.average(blocksNoGrind)
+    print("Average # of blocks when not grinding: {:.2f}".format(avg_nogrind))
+    print("Average # of blocks when grinding: {:.2f}".format(avg_grind))
+    print("-> # of blocks he won (grinding): {}".format(sum(blocksGrind)))
+    print("-> # of blocks normally won (no grinding): {}".format(sum(blocksNoGrind)))
+    print("-> adv. attacking:  {:.3f}".format(adv_attacking))
+    # adv can attack every epoch here
 
 def nico():
     maxGrindPr = 0
@@ -147,5 +201,7 @@ def nico():
     print("max probability when no grinding {:.4f}".format(maxNoGrindPr))
     print("ratio: {:.4f}".format(maxGrindPr/maxNoGrindPr))
 
-
-nico2()
+print("\nWith HS:")
+grindWithHS()
+print("\nWithout HS:")
+grindWithoutHS()
