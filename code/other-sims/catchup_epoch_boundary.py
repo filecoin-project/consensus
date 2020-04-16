fd45a6de34696d1f94f3f6625efbaf2554cf9bf7
Henri S <sternhenri@users.noreply.github.com>
2020-04-16 16:41:40 -0400

    try both methods

--- code/other-sims/catchup_epoch_boundary.py
+++ code/other-sims/catchup_epoch_boundary.py
@@ -16,26 +16,55 @@ p = e/miners
 
 s = 2**-10
 
-caughtUp = 0
-maxDistToCU = 0
-for _ in range(sims):
-    ## we can use the number from block withholding sim here as max
-    for distCatchup in range(1, 250):
-        ratBlocks = sum(np.random.binomial(ratMin, p, distCatchup))
-        advBlocks = sum(np.random.binomial(attMin, p, distCatchup))
-        
-        if advBlocks >= ratBlocks:
-            caughtUp += 1
-            maxDistToCU = max(distCatchup, maxDistToCU)
-            break
+def multiDraw():
+    caughtUp = 0
+    maxDistToCU = 0
+    for _ in range(sims):
+        ## we can use the number from block withholding sim here as max
+        for distCatchup in range(1, 250):
+            ratBlocks = sum(np.random.binomial(ratMin, p, distCatchup))
+            advBlocks = sum(np.random.binomial(attMin, p, distCatchup))
+            
+            if advBlocks >= ratBlocks:
+                caughtUp += 1
+                maxDistToCU = max(distCatchup, maxDistToCU)
+                break
+    
+    print caughtUp
+    pCatchUp = float(caughtUp)/sims
+    print "pr(catchup) = " + str(pCatchUp)
+    
+    r = 1
+    while pCatchUp**r > s:
+        r += 1
+    
+    print "secure after " + str(r) + " cycles of at most " + str(maxDistToCU) + " epochs"
 
-print caughtUp
-pCatchUp = float(caughtUp)/sims
-print "pr(catchup) = " + str(pCatchUp)
 
-r = 1
-while pCatchUp**r > s:
-    r += 1
+def singleDraw():
+    caughtUp = 0
+    maxDistToCU = 0
+    for _ in range(sims):
+        maxCatchup = 250
+        ratBlocks = np.random.binomial(ratMin, p, maxCatchup)
+        advBlocks = np.random.binomial(attMin, p, maxCatchup)
+        for distCatchup in range(1, maxCatchup):
+            if sum(advBlocks[:distCatchup]) >= sum(ratBlocks[:distCatchup]):
+                caughtUp += 1
+                maxDistToCU = max(distCatchup, maxDistToCU)
+                break
+    
+    print caughtUp
+    pCatchUp = float(caughtUp)/sims
+    print "pr(catchup) = " + str(pCatchUp)
+    
+    r = 1
+    while pCatchUp**r > s:
+        r += 1
+    
+    print "secure after " + str(r) + " cycles of at most " + str(maxDistToCU) + " epochs"
 
-print "secure after " + str(r) + " cycles of at most " + str(maxDistToCU) + " epochs"
+multiDraw()
+singleDraw()
+
 
