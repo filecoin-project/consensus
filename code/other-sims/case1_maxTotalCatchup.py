443771bd43429aea097f3a2cd31f03f4c18a07e7
Sarah Azouvi <sarah8@bitbucket.org>
2020-05-13 13:16:45 +0100

    added comments, removed case  4 not parallelised

--- code/other-sims/case1_maxTotalCatchup.py
+++ code/other-sims/case1_maxTotalCatchup.py
@@ -4,70 +4,60 @@ from math import floor
 import multiprocessing as mp
 
 
+##  In this sumlation, we compute what is the length of n consecutive
+## selfish mining attacks to get the value of maxTotalCatchUp
+
 nh=67
 na=33
 ntot=na+nh
-#heights=range(100,101,50)
-e=1
+e=5
+print "e = ", e
+Num_of_sim_per_proc = 100
 p=float(e)/float(ntot)
 
-#sim=100000
 
 ec =[]
 praos = []
 
 start_time = time.time()
-height=150
+height=150#the height is chosen to avoid infinite loop, in practice a selfish mining
+	#attack will not last 150 except with negligible probabilities
 
 def simu(sim):
 	longestfork =[]
 	win_ec = 0
-	np.random.seed()
+	np.random.seed()#initialise random seed for different processors
 	for i in range(sim):
 		ch = np.random.binomial(nh, p, height)
 		ca = np.random.binomial(na, p, height)
-		# result of flipping a coin nha times, tested 1000 times.
-		# print ch
-		# print ca
-		if e==5: num=30
-		if e==1: num=81
+		# result of flipping a coin nha times, tested height times.
+		if e==5: num=30 
+		if e==1: num=81 #number of allowed consecutive attacks computed previously
 		j=0
 		win =1
-		sumh = ch[0]
+		sumh = ch[0]#this is the begining of the selfish mining 
 		suma = ca[0]
 		ind = 1  
-		while sumh>suma and ind<height:
+		while sumh>suma and ind<height: #do attack until adversary catches up
 			sumh+=ch[ind]
 			suma+=ca[ind]
 			ind+=1
-			if ind == height:
+			if ind == height:#if adversary hasn't catch up in time, it loses
 				win =0
 				break
 		if ind <height:
 			#win = 1
 			longestfork.append(ind)
-	#print np.average(longestfork)
-	#ec.append(float(win_ec)/float(sim))
 	stop = int(floor(sim/num)*num) #need to stop before the end of the longest fork
 # #if it is not a multiple of num
-	groupedfork=[ sum(longestfork[x:x+num]) for x in range(0, stop, num)]
+	groupedfork=[ sum(longestfork[x:x+num]) for x in range(0, stop, num)]# we grouped the num
+	#successive attacks together and sums them up to get the length of num successives attacks
 	return max(groupedfork)
 
 pool = mp.Pool(mp.cpu_count())
 print mp.cpu_count()
-results = pool.map(simu, [100000000]*mp.cpu_count())
+results = pool.map(simu, [Num_of_sim_per_proc]*mp.cpu_count())
 pool.close()
-
-# print ec,np.average(longestfork), np.median(longestfork),max(longestfork), sum(longestfork[-54:])
-
-# #before sorting, we group them by groups of num
-# stop = int(floor(sim/num)*num) #need to stop before the end of the longest fork
-# #if it is not a multiple of num
-
-# groupedfork=[ sum(longestfork[x:x+num]) for x in range(0, stop, num)]
-
-
-# print ec, np.average(groupedfork), np.median(groupedfork), max(groupedfork), len(groupedfork)
 
 print results, max(results)
 
