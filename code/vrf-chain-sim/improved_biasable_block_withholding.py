3b871fe7bb4520a8ffed17b333d46c769cc0908f
Sarah Azouvi <sarah8@bitbucket.org>
2020-05-27 19:55:50 +0100

    code optimisation

--- code/vrf-chain-sim/improved_biasable_block_withholding.py
+++ code/vrf-chain-sim/improved_biasable_block_withholding.py
@@ -4,7 +4,7 @@ from math import floor
 import multiprocessing as mp
 import scipy.special
 #Initialize parameters
-Num_of_sim_per_proc = 10000
+Num_of_sim_per_proc = 100000
 height = 11 #height of the attack
 
 start_time = time.time()
@@ -25,22 +25,20 @@ def count_possibilities(ca,num):
 		s=sum(ca)
 		ca = [x for x in ca if x != 0]
 		n=len(ca)
-		l1 = [s-i for i in range(ca[-1]+1)]
+		l1 = [s-i for i in range(ca[-1]+1) if s-i>=num]
 		l = np.array(l1.copy())
 		for j in range(1,n):
 			for i in range(1,ca[-1-j]+1):
 				ll = np.array(l1)-i
+				ll = [x for x in ll if x>=num]
 				l=np.concatenate((l,ll),axis =0)
 			l1 = l.copy()
 		# dict_of_weight = {i: 0 for i in range(sum(ca)+1)}
 		# for elt in l:
 		# 	dict_of_weight[elt]+=1
 		# return dict_of_weight
-		assert len(l) == np.prod(np.array(ca)+1)
-		ct = 0
-		for node in l:
-			if node>=num:
-				ct+=1
+		#assert len(l) == np.prod(np.array(ca)+1)
+		ct = len(l)
 		# if ct>10:
 		# 	print(ca,num,l,len(l))
 		return ct
@@ -66,7 +64,7 @@ def simu(sim):
 		#diff = a_max-h_sync
 		winners = count_possibilities(ca,h_sync)
 		wa.append(winners)
-	return np.max(wa)
+	return np.average(wa)
 
 
 
@@ -75,5 +73,5 @@ print(mp.cpu_count())
 results = pool.map(simu, [Num_of_sim_per_proc]*mp.cpu_count())
 pool.close()
 
-print(results, max(results))
+print(results, np.average(results))
 print("--- %s seconds ---" % (time.time() - start_time))
